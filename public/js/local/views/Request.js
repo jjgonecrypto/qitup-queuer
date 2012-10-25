f5df6704237b1e32eef7c2a90396a3046f2c779e
justin j. moses <justinjmoses@gmail.com>
2012-10-24 23:04:25 -0400

    Cleaning up - removing redundant files

--- public/js/local/views/Request.js
+++ public/js/local/views/Request.js
@@ -1,6 +1,6 @@
 (function() {
 
-  define(["Backbone", "underscore", "collections/entries", "models/entry", "views/requests/Track", "views/requests/Artist", "views/requests/Album"], function(Backbone, _, entries, entry, Track, Artist, Album) {
+  define(["Backbone", "underscore", "collections/entries", "models/entry", "services/qitup", "text!lib/twitter-script.html"], function(Backbone, _, entries, entry, qitup, twitterScript) {
     return Backbone.View.extend({
       initialize: function() {
         var _this = this;
@@ -12,13 +12,18 @@
         });
       },
       render: function() {
-        var klazz, type;
+        var type,
+          _this = this;
         if (!this.item) return this;
         type = this.item.get("type");
-        klazz = type === "artist" ? Artist : type === "album" ? Album : Track;
-        this.$el.html(new klazz({
-          item: this.item
-        }).render().el);
+        require(["text!views/requests/" + type + ".html"], function(template) {
+          _this.$el.html(_.template(template, {
+            item: _this.item.toJSON(),
+            queue: qitup.queue(),
+            url: qitup.href()
+          }));
+          return _this.$el.append(twitterScript);
+        });
         return this;
       }
     });
